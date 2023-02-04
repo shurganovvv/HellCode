a24e5784bae84ca295afb9213ce5d08eb8224699
viktor <shurganovvv@yandex.ru>
2023-02-04 14:21:04 +0300

    add Examples004-Division_DeJleHHue project

---
+++ Examples004-Division_DeJleHHue/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberFirst = 10;
+int numberSecond = 5;
+Console.WriteLine(numberFirst / numberSecond);
+
+int number1 = 12;
+int number2 = 5;
+int result = number1 / number2;
+Console.WriteLine(result);
+
+
+double numberA = 12;
+double numberB = 5;
+double resultB = numberA / numberB;
+Console.WriteLine(resultB);
