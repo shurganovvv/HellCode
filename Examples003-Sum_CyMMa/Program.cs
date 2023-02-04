70e9c7809ff74e492abacf4eef1397044b84d6aa
viktor <shurganovvv@yandex.ru>
2023-02-04 13:54:31 +0300

    add Examples003-Sum_CyMMa project

---
+++ Examples003-Sum_CyMMa/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberFirst = 3;
+int numberSecond = 5;
+
+Console.WriteLine(numberFirst + numberSecond);
+
+
+// int numberFirst = 5;
+// int numberSecond = 2;
+int result = numberFirst + numberSecond;
+Console.WriteLine(result);
+
