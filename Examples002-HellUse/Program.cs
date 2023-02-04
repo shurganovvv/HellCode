e32c2b3ff56c32a5d795fe638130da6a880fc522
viktor <shurganovvv@yandex.ru>
2023-02-04 12:58:18 +0300

    add Examples002-HellUse project

---
+++ Examples002-HellUse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.ReadLine();
+
+Console.WriteLine("O_o");
+Console.WriteLine("Who are you? What is your name?");
+string username = Console.ReadLine();
+Console.Write("О! Здорово, ");
+Console.Write(username);
+Console.WriteLine("! Как твои дела?");
+
+Console.ReadLine();
+Console.WriteLine("To be honest, I didn't understand anything, sorry");
