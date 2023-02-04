5e48df2fdc529e759129c03a39ba5e7552b7a357
viktor <shurganovvv@yandex.ru>
2023-02-04 14:28:33 +0300

    add variables with new name in Examples003-Sum_CyMMa project

--- Examples003-Sum_CyMMa/Program.cs
+++ Examples003-Sum_CyMMa/Program.cs
@@ -6,6 +6,11 @@ Console.WriteLine(numberFirst + numberSecond);
 
 // int numberFirst = 5;
 // int numberSecond = 2;
-int result = numberFirst + numberSecond;
-Console.WriteLine(result);
+int resultA = numberFirst + numberSecond;
+Console.WriteLine(resultA);
 
+
+int number1 = 5;
+int number2 = 2;
+int result2 = number1 + number2;
+Console.WriteLine(result2);
