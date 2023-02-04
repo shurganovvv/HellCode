62cce07bd6bb24e71d055e7bb4a8f30c7971c712
viktor <shurganovvv@yandex.ru>
2023-02-04 18:16:32 +0300

    add Examples005-Conditions_if_else-yCJIOBue_ECJIu project

---
+++ Examples005-Conditions_if_else-yCJIOBue_ECJIu/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Вы кто такие?");
+string username = Console.ReadLine();
+if (username.ToLower() == "карлсон")
+    {
+        Console.Write("Карлсончик, дорогой!");
+    }
+    else {
+        Console.Write("Я Вас, ");
+        Console.Write(username);
+        Console.WriteLine(", не знаю.");
+    }
