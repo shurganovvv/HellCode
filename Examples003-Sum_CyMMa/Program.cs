0a26d4dc34e1ecbd2276cfd633ac3ec9a22d8db8
viktor <shurganovvv@yandex.ru>
2023-02-04 16:13:12 +0300

    add Random in Examples003-Sum_CyMMa project

--- Examples003-Sum_CyMMa/Program.cs
+++ Examples003-Sum_CyMMa/Program.cs
@@ -14,3 +14,13 @@ int number1 = 5;
 int number2 = 2;
 int result2 = number1 + number2;
 Console.WriteLine(result2);
+
+Console.WriteLine();
+int numberA = new Random().Next(1,10); // 1,2,3,4,5,6,7,8,9
+int numberB = new Random().Next(1,10);
+int resultB = numberA + numberB;
+Console.Write(numberA);
+Console.Write(" + ");
+Console.Write(numberB);
+Console.Write(" = ");
+Console.WriteLine(resultB);
